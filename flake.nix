7d8e26823c15cb2f316c11b27bccc17e799aaba7
ojii3 <84656786+OJII3@users.noreply.github.com>
2026-01-11 02:25:47 +0900

    Add Nix flake support
    - Add flake.nix with package definition for all platforms
    - Add overlay for easy integration with nixpkgs
    - Add MIT LICENSE
    - Add .gitignore for nix build artifacts

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "HI3 Starfall - Obsidian theme inspired by Honkai Impact 3rd";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.stdenvNoCC.mkDerivation {
+            pname = "obsidian-hi3-theme";
+            version = "1.0.0";
+
+            src = ./.;
+
+            installPhase = ''
+              runHook preInstall
+              mkdir -p $out/share/obsidian/themes/HI3\ Starfall
+              cp manifest.json theme.css $out/share/obsidian/themes/HI3\ Starfall/
+              runHook postInstall
+            '';
+
+            meta = with pkgs.lib; {
+              description = "Obsidian theme inspired by Honkai Impact 3rd 'Starfall'";
+              homepage = "https://github.com/OJII3/obsidian-hi3-theme";
+              license = licenses.mit;
+              maintainers = [ ];
+              platforms = platforms.all;
+            };
+          };
+        }
+      );
+
+      overlays.default = final: prev: {
+        obsidian-hi3-theme = self.packages.${final.system}.default;
+      };
+    };
+}
